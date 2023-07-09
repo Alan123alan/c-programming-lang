e7fce0d6592ba9bb8a9eb8d399e8956f126ab54f
kmnj500 <alancastellanos2@hotmail.com>
2023-07-09 10:15:11 -0700

    Added header to Fahrenheit - Celsius convertion table

--- fahr_to_celsius.c
+++ fahr_to_celsius.c
@@ -5,9 +5,10 @@ int lower_limit, upper_limit;
 float celsius;
 lower_limit = 0;
 upper_limit = 300;
+printf("Fahrenheit Celsius\n");
 for(int fahr = lower_limit;fahr <= upper_limit;fahr += 20){
 celsius = (5.0/9.0)*(fahr-32);
-printf("%3d %6.1f\n",fahr,celsius);
+printf("%10d %7.1f\n",fahr,celsius);
 }
 
 return 0;
