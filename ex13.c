03370e4ab28abd47ec4089adb2066bcc191df6b1
Alan123alan <alancastellanos2@hotmail.com>
2023-08-13 14:37:40 -0700

    Added code for exercise 13 'Extra credit' point 4: assign element from states to argv and the inverse

--- ex13.c
+++ ex13.c
@@ -4,14 +4,19 @@ int main(int argc, char *argv[])
 {
 	// go through each string in argv
 
+	// let's make our own array of strings
+	// extra credit point 3, add NULL to states strings array
+	char *states[] = { "Oregon", "California", "Arizona", "Colorado", NULL};
+
+	// extra credit point 4, assign an element from states array to the argv and the inverse
+	argv[1] = states[1];
+	states[0] = argv[0];
+
 	// why am I skipping over argv[0]? because argv[0] is the program name
 	// how to break it point 2, initialize i to 0
 	for(int i = 0; i < argc; i++){//initializing to 0 works because the first argv is the program name
 		printf("arg #%d: %s.\n", i, argv[i]);
 	}
-	// let's make our own array of strings
-	// extra credit point 3, add NULL to states strings array
-	char *states[] = { "Oregon", "California", "Arizona", "Colorado", NULL};
 	// how to break it point 3, set num_of_states to a value higher to the elements contained in states strings array
 	int num_of_states = 5;
 	for(int i = 0; i < num_of_states; i++)
