d0e5ca4baa63ce17b732a0e48e7b8357f1a777a2
Alan123alan <alancastellanos2@hotmail.com>
2023-08-24 22:38:04 -0600

    Added code for exercise 15 'Extra Credit' point 2: rewrite all of the pointers in this program as arrays

--- ex15.c
+++ ex15.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	// How to break it point #2, invert all loops	
-
 	// create two arrays we care about
 	// Extra credit point 1 rewrite all arrays as pointers
-	int ages_arr[] = { 23, 43, 12, 89, 2};
-	int *ages = ages_arr; 
-	char *names_arr[] = { "Alan", "Frank", "Mary", "John", "Lisa"};
-	char **names = names_arr;
+	// Extra credit point 2 rewrite all pointers as arrays
+	int ages[] = { 23, 43, 12, 89, 2};
+	char names[5][5] = { "Alan", "Frank", "Mary", "John", "Lisa"};
 
-	// ages will be the address of the first int in ages
-	//printf("ages: %d.\n", ages);
-	
-	// since ages is already the address with dereference operator we should get the value
-	//printf("ages: %d.\n", *ages);
-
-	// then what is returned if we use the address of data operator??
-	// my guess is that it will print the same address as just printing ages
-	//printf("ages: %d.\n", &ages);
 
 	// what does names point to? "Alan"? or just "A"?
 	printf("names evaluates to the address: %p.\n", (void *) names);// got an address, need to dereference
 	printf("the value pointed at by the names address: %p.\n", (void *) *names);// it prints another address, so it's an array of pointers
 	// and this pointers point to the first pointer of the char array, can I get the address of this char array?
 	//what is the size of names?
-	int names_size = sizeof(names_arr);
-	int names_count = sizeof(names_arr)/sizeof(names);
+	int names_size = sizeof(names);
+	int names_count = sizeof(names)/sizeof(names);
 	//int first_str_size = sizeof(*names)/sizeof(char);
 	printf("size of names = %d.\n", names_size);
 	printf("count of names = %d.\n", names_count);
@@ -35,7 +25,7 @@ int main(int argc, char *argv[])
 	printf("value of first string? : %c.\n", *(*names));
 	
 	// safely get the size of ages
-	int count = sizeof(ages_arr)/sizeof(int);
+	int count = sizeof(ages)/sizeof(ages[0]);
 	
 	// first way using indexing
 	for(int i = count-1; i >= 0; i--){
@@ -44,36 +34,30 @@ int main(int argc, char *argv[])
 	printf("---\n");
 	
 	// set up the pointers to the start of the arrays
-	int *curr_age = ages;
-	char **curr_name = names;
+	int curr_age[count];
+	char curr_name[5][5];
 	
+	for(int i = 0; i < count; i++){
+		curr_age[i] = ages[i];
+		strcpy(curr_name[i], names[i]);
+	}
 	
 	// second way using pointers
 	for(int i = count-1; i >= 0; i--){
-		printf("%s is %d years old.\n", *(curr_name + i), *(curr_age + i));
+		printf("%s is %d years old.\n", curr_name[i], curr_age[i]);
 	}
 
 	printf("---\n");
 	
 	// third way, pointers are just arrays
-	printf("size of ages (in bytes) to which curr_age points to: %ld == %d ints.\n", sizeof(ages_arr), count);
+	printf("size of ages (in bytes) to which curr_age points to: %ld == %d ints.\n", sizeof(ages), count);
 	for(int i = count-1; i >= 0; i--){
-		// A difference between pointers and arrays sizeof pointers just returns the size of pointer type
+		// A difference between pointers and arrays, sizeof pointers just returns the size of pointer type
 		// sizeof an array returns the size of the whole array
 		printf("size of curr_age int pointer: %ld.\n", sizeof(curr_name));
 		printf("%s is %d years old.\n", curr_name[i], curr_age[i]);
 	}
 	
 	printf("---\n");
-
-	// fourth way with pointers in a stupid complex way
-	for(curr_age = ages + (count - 1), curr_name = names + (names_count - 1); (curr_age - ages) >= 0; curr_age--,curr_name--){
-		printf("%s is %d years old.\n",*curr_name, *curr_age);
-	}
-
-	
-	// How to break it point #1
-	//curr_age = (int**)names;
-	//printf("curr_age %d %s.\n", &curr_age, *curr_age);
 	return 0;
 }
