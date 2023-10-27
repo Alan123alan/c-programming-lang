d6984d1cebe85bd6abf3574ddef026fe976d57c0
Alan123alan <alancastellanos2@hotmail.com>
2023-10-26 19:01:43 -0600

    added exercism tests for square root exercise

--- square_root/square_root.c
+++ square_root/square_root.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <math.h>
-#include <stdlib.h>
+#include "square_root.h"
 
-float square_root(unsigned int radicand){
+uint16_t square_root(uint16_t radicand){
     float approximation = (float)radicand;
-    printf("Initial approximation for square root: %f.\n", approximation);
+    // printf("Initial approximation for square root: %f.\n", approximation);
     float precission = pow(10, -10);
-    printf("Precission for square root calculation: %f.\n", precission);
+    // printf("Precission for square root calculation: %f.\n", precission);
     while(fabs(radicand - (float)pow(approximation, 2)) > precission){
         approximation = (approximation + (float)radicand/approximation)/2.0;
-    	printf("Approximation for square root: %f.\n", approximation);
+    	// printf("Approximation for square root: %f.\n", approximation);
     }
-    return approximation;
+    return (uint16_t)approximation;
 }
 
-int main(int argc, char *argv[]){
-    printf("Square root approximation for radicand = 4 -> %f.\n", square_root(4));
-}
+// int main(int argc, char *argv[]){
+//     printf("Square root approximation for radicand = 4 -> %f.\n", square_root(4));
+// }
---
+++ square_root/square_root.h
@@ -0,0 +1,13 @@
+#ifndef SQUARE_ROOT_H
+#define SQUARE_ROOT_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+uint16_t square_root(uint16_t radicand);
+
+
+#endif
